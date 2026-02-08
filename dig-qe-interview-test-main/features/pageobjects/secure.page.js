1aca4e827614a941a957c38b81c161ddcf1788ce
gonepudiramesh-source <gonepudiramesh@gmail.com>
2026-02-08 21:32:33 +0530

    Delete dig-qe-interview-test-main directory

--- dig-qe-interview-test-main/features/pageobjects/secure.page.js
+++
@@ -1,8 +0,0 @@
-import { $ } from "@wdio/globals";
-import BasePage from "./base.page.js";
-
-class SecurePage extends BasePage {
-  get header() { return $("h2"); }
-}
-
-export default new SecurePage();
